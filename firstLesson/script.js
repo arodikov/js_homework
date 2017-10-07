8a9001769469720eaa5ee308cd40ea81036ec04a
arodikov <aaa>
2017-10-07 23:46:20 +0300

    tasks 2-4

--- firstLesson/script.js
+++ firstLesson/script.js
@@ -1,13 +1,65 @@
-function parseAmount(amount) {
-
+function parseAmount(ama) {
   let amaReg = /^\d{1,}$|^\d{1,},\d{1,2}$/;
+  if (ama.match(amaReg)) {
+    alert(parseFloat(ama.replace(',', '.')) * 100);
+  } else {
+    alert("Некорректная сумма");
+  }
+}
 
-  if (amount.match(amaReg)) {
-      alert(parseFloat(amount.replace(',', '.')) * 100);
+function convertString(str) {
+  let newStr = '';
+  for (i = 0; i < str.length; i++) {
+    let count = str.toLowerCase().split(str[i].toLowerCase()).length - 1;
+    if (count > 1) {
+      newStr = newStr + ')';
     } else {
-      alert("Некорректная сумма");
+      newStr = newStr + '(';
     }
   }
+  alert(newStr);
+}
 
-  let ama = prompt("Введите сумму:");
-  parseAmount(ama);
+function sevenAte9(str) {
+  while (str.indexOf("797") != -1) {
+    str = str.replace(/797/g, "77");
+  }
+  alert(str);
+}
+
+function dna(str) {
+  str = str.replace(/\w/g, replacer);
+  alert(str);
+}
+
+function replacer(str) {
+  switch (str.toUpperCase()) {
+    case 'A':
+      return 'T';
+      break;
+    case 'T':
+      return 'A';
+      break;
+    case 'C':
+      return 'G';
+      break;
+    case 'G':
+      return 'C';
+      break;
+    default:
+      return str;
+      break;
+  }
+}
+
+let ama = prompt("Задание 1 (перевод в минорные). Введите сумму:");
+parseAmount(ama);
+
+let str = prompt("Задание 2 (замена на скобки). Введите строку:");
+convertString(str);
+
+let str7ate9 = prompt("Задание 3 (797 на 77). Введите число:");
+sevenAte9(str7ate9);
+
+let strDNA = prompt("Задание 4 (ДНК). Введите последовательность:");
+dna(strDNA);
